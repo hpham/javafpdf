e39cda2100c40c5db3b4afcbae3dac866dd212a2
Hanh <hanh.pham@t-online.de>
2015-10-07 21:37:45 +0200

    svn import commit

---
+++ src/net/sourceforge/javafpdf/Layout.java
@@ -0,0 +1,19 @@
+package net.sourceforge.javafpdf;
+
+/**
+ * PDF display layout.
+ * 
+ * @author Alan Plum
+ * @since 4 Mar 2008
+ * @version $Rev: 8 $
+ */
+public enum Layout {
+	/** Single page layout. */
+	SINGLE,
+	/** Continuous layout. */
+	CONTINUOUS,
+	/** Two pages layout. */
+	TWO,
+	/** Default layout. */
+	DEFAULT;
+}
---
+++ src/net/sourceforge/javafpdf/Zoom.java
@@ -0,0 +1,19 @@
+package net.sourceforge.javafpdf;
+
+/**
+ * PDF zoom mode.
+ * 
+ * @author Alan Plum
+ * @since 4 Mar 2008
+ * @version $Rev: 8 $
+ */
+public enum Zoom {
+	/** Full page zoom. */
+	FULLPAGE,
+	/** Full width zoom. */
+	FULLWIDTH,
+	/** Real zoom. */
+	REAL,
+	/** Default zoom. */
+	DEFAULT;
+}
