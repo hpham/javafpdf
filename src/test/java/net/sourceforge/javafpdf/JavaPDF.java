483ce212d20678faa5265310fa2748b5587536a8
Hanh <hanh.pham@t-online.de>
2015-10-07 22:23:10 +0200

    minimal test

---
+++ src/test/java/net/sourceforge/javafpdf/JavaPDF.java
@@ -0,0 +1,21 @@
+package net.sourceforge.javafpdf;
+
+public class JavaPDF extends FPDF {
+
+	public JavaPDF() {
+		super();
+	}
+
+	@Override
+	public void Footer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Header() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/net/sourceforge/javafpdf/MinimalTest.java
+++ src/test/java/net/sourceforge/javafpdf/MinimalTest.java
@@ -16,7 +16,23 @@ public class MinimalTest {
 		Set<FontStyle> set = new HashSet<FontStyle>();
 		pdf.setFont("Arial", set, 16);
 		pdf.Cell(40, 10, "Hello World!");
-		File temp = new File("c:/temp/test.pdf");
+		File temp = File.createTempFile("fpdf", ".pdf");
 		pdf.output(temp);
+		System.out.println(temp.getAbsolutePath());
+	}
+
+	@Test
+	public void test_header_footer_pdf() throws IOException {
+		FPDF pdf = new JavaPDF();
+		pdf.aliasNbPages();
+		pdf.addPage();
+		Set<FontStyle> set = new HashSet<FontStyle>();
+		pdf.setFont("Times", set, 12);
+		for (int i = 1; i <= 40; i++) {
+			pdf.Cell(0, 10, "Printing line number " + i, Position.BELOW, Alignment.LEFT);
+		}
+		File temp = File.createTempFile("fpdf", ".pdf");		
+		pdf.output(temp);
+		System.out.println(temp.getAbsolutePath());
 	}
 }
